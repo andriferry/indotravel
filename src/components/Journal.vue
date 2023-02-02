554104c0ab0a7b2bc3e1376ef4fb8b1e1dd028ef
andriferry <ferryandri35@gmail.com>
2023-02-02 11:11:35 +0700

    Update journal

--- src/components/Journal.vue
+++ src/components/Journal.vue
@@ -21,6 +21,31 @@ const spots = ref(
   ]
 );
 
+const articles = ref(
+  [
+    {
+      date: 'Feb 20, 2022',
+      text: 'Borobudur is an Indonesian monument and is one of those places one should visit before they die.',
+      img: "bg-[url('https://images.unsplash.com/photo-1631340729644-8b8aad1e9dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')]"
+    },
+    {
+      date: "Jan 16, 2022",
+      text: "Watching the sunrise from top of Mount Batur, which is very much an active volcano.",
+      img: "bg-[url('https://images.unsplash.com/photo-1616584743489-41c917198ee6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')]"
+    },
+    {
+      date: "Jan 10, 2023",
+      text: "Get to know these 10+ super delicious original foods from Indonesia.",
+      img: "bg-[url('https://images.unsplash.com/photo-1635809846316-bc4d0f0a0049?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80')]"
+    },
+    {
+      date: "Aug 15, 2022",
+      text: "Meet Raja Ampat, a world's heaven that you can only find in Indonesia.",
+      img: "bg-[url('https://images.unsplash.com/photo-1570789210967-2cac24afeb00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80')]"
+    }
+  ]
+)
+
 const flicking = ref()
 const breakpoints = useBreakpoints(breakpointsTailwind)
 
@@ -61,22 +86,22 @@ watch(breakpoints.sm, (value) => {
       <div class="flex justify-center w-full">
         <div class="w-[350px] sm:w-full">
           <Flicking ref="flicking" :options="options">
-            <div class="" v-for="data in 5" :key="data">
+            <div class="" v-for="(data , index) in articles" :key="index">
               <div class="flex gap-3 proseriesLandscape:w-[400px] xl:w-[628px] justify-center flex-col">
                 <div class="flex flex-col w-[350px] xl:w-full px-5 proseries:px-0 sm:px-2">
                   <span class="font-normal text-xs xl:text-sm text-[#E7ECF0] tracking-wide leading-6">
-                    FEB 20, 2022
+                    {{data.date}}
                   </span>
 
                   <p class="leading-7 xl:leading-9 font-semibold text-base xl:text-2xl">
-                    Borobudur is an Indonesian monument and is one of those places one should visitbefore    they die.
+                    {{data.text}}
                   </p>
                 </div>
 
                 <div class="w-full proseries:px-2 flex justify-center sm:justify-start">
                   <div 
                     class="w-[312px] proseries:w-full sm:px-1 h-[200px] md:h-[367px] xl:h-[418px] bg-cover bg-center rounded-xl" 
-                    :class="bgImg" 
+                    :class="data.img" 
                   />
                 </div>
               </div>
