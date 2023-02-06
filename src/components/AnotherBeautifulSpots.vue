dc8355cf8975e488896d767b2146562bdc516399
andriferry <ferryandri35@gmail.com>
2023-02-06 21:21:43 +0700

    update another spots

--- src/components/AnotherBeautifulSpots.vue
+++ src/components/AnotherBeautifulSpots.vue
@@ -5,7 +5,8 @@ import { ref } from 'vue'
 
 <template>
   <div class="text-white mt-20 w-100 h-100 flex justify-center items-center flex-col gap-10">
-    <p class="text-xl">
+    <p class="text-2xl sm:text-[32px] lg:text-[40px] sm:w-[358px] lg:w-[409px]
+    leading-10 sm:leading-[54px] lg:leading-[64px] font-semibold text-center">
       ANOTHER BEAUTIFUL VIEW OF INDONESIA
     </p>
   </div>
